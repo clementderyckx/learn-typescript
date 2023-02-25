2209d629d4dec71e29eb7d12724e9f294d07d670
Clement Deryckx <clement.deryckx@outlook.com>
2023-02-25 22:12:25 +0100

    end part1

---
+++ app.ts
@@ -0,0 +1,10 @@
+// interface Todo {
+//   id: number,
+//   title: string,
+//   completed: boolean,
+// }
+
+// const task = { id: 1, title: 'Buy milk', completed: false } as Todo;
+// console.log(task);
+
+console.log('hey');
